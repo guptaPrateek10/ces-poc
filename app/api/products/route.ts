496270849d652d7fd0344d6c73f9f01e279a1b14
Prateek Gupta <prateek.gupta@cesltd.com>
2025-01-23 15:22:56 +0530

    addressed the comments

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,12 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export async function GET() {
   const res = await fetch('https://fakestoreapi.com/products')
   const data = await res.json();
-  //logic
   return Response.json(data)
 }
 
 export async function POST(request: Request) {
   const res = await request.json();
   return Response.json({ res });
-  
 }
--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,10 @@ export async function getProducts(): Promise<ProductTypes[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
     cache: "no-store",
   });
+  
   if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
-  
+
   return res.json();
 }
